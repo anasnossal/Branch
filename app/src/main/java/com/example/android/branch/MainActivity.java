9dac1a90db529da5aa68674edfb4d149e20812c1
anastasianossal <an2832@tc.columbia.edu>
2018-03-05 22:26:08 -0500

    Added int and String to a NewMethod()

--- app/src/main/java/com/example/android/branch/MainActivity.java
+++ app/src/main/java/com/example/android/branch/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void NewMethod()
+    {
+        int i = 2+2;
+        String test = "test";
+    }
 }
